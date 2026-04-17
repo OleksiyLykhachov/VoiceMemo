dac5d3d4804a68fda1cfc49455d7730b7d9cce95
arl <arl@anadeainc.com>
2026-04-17 22:45:44 +0300

    feat: RecordButton

---
+++ lib/presentation/styles/voice_memos_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class VoiceMemosColors {
+  static const red = Color(0xffF83700);
+  static const black = Colors.black;
+  static const textDark = Color(0xff010101);
+  static const white = Colors.white;
+  static const textTertiary = Color(0xffA4A6A7);
+  static const textSecondary = Color(0xffA0A2A3);
+}
